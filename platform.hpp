4e67b9d29bc071dcecb7bd5f4b9f67d9faddfacc
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-04-25 20:55:03 +0300

    Implement platform simulation

---
+++ platform.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <cstdint>
+#include <optional>
+
+class Platform
+{
+public:
+	Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight)
+		: window(sf::VideoMode({ static_cast<unsigned int>(windowWidth), static_cast<unsigned int>(windowHeight) }), title),
+		texture(),
+		sprite(texture)
+	{
+		(void)texture.resize(sf::Vector2u(static_cast<unsigned int>(textureWidth), static_cast<unsigned int>(textureHeight)));
+		sprite.setTexture(texture, true);
+
+		float scaleX = static_cast<float>(windowWidth) / static_cast<float>(textureWidth);
+		float scaleY = static_cast<float>(windowHeight) / static_cast<float>(textureHeight);
+		sprite.setScale(sf::Vector2f(scaleX, scaleY));
+	}
+
+	~Platform()
+	{
+	}
+
+	void Update(void const* buffer, int pitch)
+	{
+		texture.update(static_cast<const std::uint8_t*>(buffer));
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+
+	bool ProcessInput(uint8_t* keys)
+	{
+		bool quit = false;
+
+		while (std::optional<sf::Event> event = window.pollEvent())
+		{
+			if (event->getIf<sf::Event::Closed>())
+			{
+				quit = true;
+			}
+			else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+			{
+				switch (keyPressed->code)
+				{
+				case sf::Keyboard::Key::Escape: quit = true; break;
+				case sf::Keyboard::Key::X: keys[0] = 1; break;
+				case sf::Keyboard::Key::Num1: keys[1] = 1; break;
+				case sf::Keyboard::Key::Num2: keys[2] = 1; break;
+				case sf::Keyboard::Key::Num3: keys[3] = 1; break;
+				case sf::Keyboard::Key::Q: keys[4] = 1; break;
+				case sf::Keyboard::Key::W: keys[5] = 1; break;
+				case sf::Keyboard::Key::E: keys[6] = 1; break;
+				case sf::Keyboard::Key::A: keys[7] = 1; break;
+				case sf::Keyboard::Key::S: keys[8] = 1; break;
+				case sf::Keyboard::Key::D: keys[9] = 1; break;
+				case sf::Keyboard::Key::Z: keys[0xA] = 1; break;
+				case sf::Keyboard::Key::C: keys[0xB] = 1; break;
+				case sf::Keyboard::Key::Num4: keys[0xC] = 1; break;
+				case sf::Keyboard::Key::R: keys[0xD] = 1; break;
+				case sf::Keyboard::Key::F: keys[0xE] = 1; break;
+				case sf::Keyboard::Key::V: keys[0xF] = 1; break;
+				default: break;
+				}
+			}
+			else if (const auto* keyReleased = event->getIf<sf::Event::KeyReleased>())
+			{
+				switch (keyReleased->code)
+				{
+				case sf::Keyboard::Key::X: keys[0] = 0; break;
+				case sf::Keyboard::Key::Num1: keys[1] = 0; break;
+				case sf::Keyboard::Key::Num2: keys[2] = 0; break;
+				case sf::Keyboard::Key::Num3: keys[3] = 0; break;
+				case sf::Keyboard::Key::Q: keys[4] = 0; break;
+				case sf::Keyboard::Key::W: keys[5] = 0; break;
+				case sf::Keyboard::Key::E: keys[6] = 0; break;
+				case sf::Keyboard::Key::A: keys[7] = 0; break;
+				case sf::Keyboard::Key::S: keys[8] = 0; break;
+				case sf::Keyboard::Key::D: keys[9] = 0; break;
+				case sf::Keyboard::Key::Z: keys[0xA] = 0; break;
+				case sf::Keyboard::Key::C: keys[0xB] = 0; break;
+				case sf::Keyboard::Key::Num4: keys[0xC] = 0; break;
+				case sf::Keyboard::Key::R: keys[0xD] = 0; break;
+				case sf::Keyboard::Key::F: keys[0xE] = 0; break;
+				case sf::Keyboard::Key::V: keys[0xF] = 0; break;
+				default: break;
+				}
+			}
+		}
+
+		return quit;
+	}
+
+private:
+	sf::RenderWindow window;
+	sf::Texture texture;
+	sf::Sprite sprite;
+};
